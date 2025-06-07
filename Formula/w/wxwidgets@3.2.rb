6e986c34a21d1d83a9d4e4b83cbc1eda9d6b9ac3
Daeho Ro <40587651+daeho-ro@users.noreply.github.com>
2025-06-07 14:11:10 +0900

    wxwidgets@3.2 3.2.8.1 (new formula)

---
+++ Formula/w/wxwidgets@3.2.rb
@@ -0,0 +1,108 @@
+class WxwidgetsAT32 < Formula
+  desc "Cross-platform C++ GUI toolkit"
+  homepage "https://www.wxwidgets.org"
+  url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.8.1/wxWidgets-3.2.8.1.tar.bz2"
+  sha256 "ad0cf6c18815dcf1a6a89ad3c3d21a306cd7b5d99a602f77372ef1d92cb7d756"
+  license "LGPL-2.0-or-later" => { with: "WxWindows-exception-3.1" }
+  head "https://github.com/wxWidgets/wxWidgets.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(3\.2(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any,                 arm64_sequoia: "5e0aa82886721db8bc109785272271aecbc86b2e44185bacb6c947fd19f6fcc2"
+    sha256 cellar: :any,                 arm64_sonoma:  "c8e1a68822e6a854138ed7d17be706a07df64e4a38900b98a6e88a36f785c500"
+    sha256 cellar: :any,                 arm64_ventura: "5d9b32b6973e71173101c6b45d1a350f724442088681b5721caa613318805876"
+    sha256 cellar: :any,                 sonoma:        "981feac70f9e659e8e45d1b7de4030fcab77ecda3dab2c0d43033e116d24122c"
+    sha256 cellar: :any,                 ventura:       "ef48295f3fdc268a6e1ed3daef93017646105dff9bb962f67b567d66830c451c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "19d1970a8f32864712fa40f2932e943a097ac9752b04f78fdf912a86b3d6bb11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40a393ed11e8f3dee0153c52441eec44b185c197a2c2175a7d592105aa0df5b9"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "pkgconf" => :build
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "pcre2"
+
+  uses_from_macos "expat"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "cairo"
+    depends_on "fontconfig"
+    depends_on "gdk-pixbuf"
+    depends_on "glib"
+    depends_on "gtk+3"
+    depends_on "libsm"
+    depends_on "libx11"
+    depends_on "libxkbcommon"
+    depends_on "libxtst"
+    depends_on "libxxf86vm"
+    depends_on "mesa"
+    depends_on "mesa-glu"
+    depends_on "pango"
+    depends_on "wayland"
+  end
+
+  def install
+    # Remove all bundled libraries excluding `nanosvg` which isn't available as formula
+    %w[catch pcre].each { |l| rm_r(buildpath/"3rdparty"/l) }
+    %w[expat jpeg png tiff zlib].each { |l| rm_r(buildpath/"src"/l) }
+
+    args = [
+      "--enable-clipboard",
+      "--enable-controls",
+      "--enable-dataviewctrl",
+      "--enable-display",
+      "--enable-dnd",
+      "--enable-graphics_ctx",
+      "--enable-std_string",
+      "--enable-svg",
+      "--enable-unicode",
+      "--enable-webviewwebkit",
+      "--with-expat",
+      "--with-libjpeg",
+      "--with-libpng",
+      "--with-libtiff",
+      "--with-opengl",
+      "--with-zlib",
+      "--disable-tests",
+      "--disable-precomp-headers",
+      # This is the default option, but be explicit
+      "--disable-monolithic",
+    ]
+
+    if OS.mac?
+      # Set with-macosx-version-min to avoid configure defaulting to 10.5
+      args << "--with-macosx-version-min=#{MacOS.version}"
+      args << "--with-osx_cocoa"
+      args << "--with-libiconv"
+
+      # Work around deprecated Carbon API, see
+      # https://github.com/wxWidgets/wxWidgets/issues/24724
+      inreplace "src/osx/carbon/dcscreen.cpp", "#if !wxOSX_USE_IPHONE", "#if 0" if MacOS.version >= :sequoia
+    end
+
+    system "./configure", *args, *std_configure_args
+    system "make", "install"
+
+    # wx-config should reference the public prefix, not wxwidgets's keg
+    # this ensures that Python software trying to locate wxpython headers
+    # using wx-config can find both wxwidgets and wxpython headers,
+    # which are linked to the same place
+    inreplace bin/"wx-config", prefix, HOMEBREW_PREFIX
+
+    # For consistency with the versioned wxwidgets formulae
+    bin.install_symlink bin/"wx-config" => "wx-config-#{version.major_minor}"
+    (share/"wx"/version.major_minor).install share/"aclocal", share/"bakefile"
+  end
+
+  test do
+    system bin/"wx-config", "--libs"
+  end
+end
